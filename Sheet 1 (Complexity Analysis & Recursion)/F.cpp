14361b635d6b58a0d292d66aa7a913fcb1ed0903
cewatlac <128319370+cewatlac@users.noreply.github.com>
2024-11-05 21:17:36 +0200

    Create F.cpp

---
+++ Sheet 1 (Complexity Analysis & Recursion)/F.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int x , y , z;
+    cin>> x >> y >> z;
+    cout << z << " " << x << " " << y;
+    return 0;
+}
